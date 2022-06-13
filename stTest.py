d057a94af457486b5f0d25687e77aa393b656b28
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2022-06-13 10:06:42 +0200

    Create stTest.py

---
+++ stTest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+import streamlit as st
+import pandas as pd
+
+st.write("""
+    # Kleiner Test
+    
+    mit diesem Kram
+    """)
+
+pd.read_csv('https://raw.githubusercontent.com/Hezel2000/GeoDataScience/main/data/moessbauer%20standard%20data.csv')
+
+
+# In[ ]:
+
+
+
+
