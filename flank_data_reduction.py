12a29c45426dfb03cac7f49d9bf0e096fb967344
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2022-06-18 22:12:33 +0200

    Update flank_data_reduction.py

--- flank_data_reduction.py
+++ flank_data_reduction.py
@@ -517,7 +517,7 @@ def visualisations():
         
         elements = st.session_state.dfMain.columns.tolist()[3:]
         
-        if sel == 'elements':
+        if sel == 'Composition of drift standards':
             el = st.selectbox('Select', elements)
     
             av = np.average(st.session_state.dfdr[el])
@@ -525,15 +525,10 @@ def visualisations():
             
             reldev = 100 * np.std(st.session_state.dfdr[el])/np.average(st.session_state.dfdr[el])
         #                elif sel == 'elements  ':
-            if reldev < 1:
-                fcColor = (.5, 0.8, 0)
-            elif 1 <= reldev < 5:
-                fcColor = 'orange'
-            else:
-                fcColor = 'r'
+    
               
             col1, col2 = st.columns([3, 1])
-            col1.subheader('Drift Monitor Visualisation')
+            col1.subheader('Drift Monitor')
             #st.write(st.session_state.dfdr.to_dict())
             
             TOOLTIPS = [('Name', '@Name'),
@@ -545,7 +540,7 @@ def visualisations():
             fig.line(st.session_state.dfdr.loc[:, 'Point Nr.'], st.session_state.dfdr.loc[:, el])
             #fig.circle(st.session_state.dfdr.loc[:, 'Point Nr.'], st.session_state.dfdr.loc[:, el])
             output_file("toolbar.html")  
-            source = ColumnDataSource(data = st.session_state.dfdr.to_dict('list'))
+            source = ColumnDataSource(st.session_state.dfdr.to_dict('list'))
             
             fig.circle('Point Nr.', el, size=4, source=source)
         
@@ -566,26 +561,27 @@ def visualisations():
             resRelStd ='rel. std.: ' + str(round(reldev, 2)) + '%'
             col2.write(resAv)
             col2.write(resRelStd)
-            redDot = "https://static.streamlit.io/examples/dice.jpg"
-            redCaption = 'worrisome data'
-            greenDot = 'https://github.com/Hezel2000/microprobe/blob/2c4fa6b09e359b1abd7346d6c054a4d9d2d408f3/red_dot.svg'
-            col2.image(redDot, caption = redCaption, width=64)
-            st.color_picker('worst', '#FF0000')
+            if reldev < 1:
+                col2.color_picker('good data', '#39EC39')
+            elif 1 <= reldev < 5:
+                col2.color_picker('check data', '#F7CF0F')
+            else:
+                col2.color_picker('worrisome data', '#FF0000')
         
         else:
-            dfdrLRatioTAP2 = st.session_state.dfdr[r'L$\beta$ (TAP2)']/st.session_state.dfdr[r'L$\alpha$ (TAP2)']
-            dfdrLRatioTAP4 = st.session_state.dfdr[r'L$\beta$ (TAP4)']/st.session_state.dfdr[r'L$\alpha$ (TAP4)']
-            dfdrCalList = pd.concat([st.session_state.dfdr['FeO'], dfdrLRatioTAP2], axis = 1)
-            dfdrCalList = dfdrCalList.rename(columns = {'FeO': r'Fe$_{tot}$', 0:r'L$\beta$/L$\alpha$ (TAP2)'})
-            dfdrCalList4 = pd.concat([st.session_state.dfdr['FeO'], dfdrLRatioTAP4], axis = 1)
-            dfdrCalList4 = dfdrCalList4.rename(columns = {'FeO': r'Fe$_{tot}$', 0:r'L$\beta$/L$\alpha$ (TAP4)'})
-    
             fig = figure(width=600, height=400)
-            fig.scatter(regressionFitParameters(dfdrCalList, 'TAP2'),
-                        regressionFitParameters(dfdrCalList4, 'TAP4'))
+            fig.scatter(st.session_state.resultsFe3Drift['Fe$^{3+}$/$\Sigma$Fe (2TAPL)'],
+                        st.session_state.resultsFe3Drift['Fe$^{3+}$/$\Sigma$Fe (4TAPL)'])
             fig.xaxis.axis_label=r'''Fe$^{3+}$/$\Sigma$Fe (FP, TAP2)'''
             fig.yaxis.axis_label=r'Fe$^{3+}$/$\Sigma$Fe (FP, TAP4)'
+            
+            x = np.linspace(0,1.1 * st.session_state.resultsFe3Drift['Fe$^{3+}$/$\Sigma$Fe (2TAPL)'].max(),10)
+            fig.line(x, x)
+            fig.line(x, x + .01, line_dash='dashed', line_color='orange')
+            fig.line(x, x - .01, line_dash='dashed', line_color='orange')
+            
             st.bokeh_chart(fig)
+            
             
 #--------  End Drift Inspection
 
@@ -596,11 +592,16 @@ def visualisations():
         #from bokeh.models import Span, BoxAnnotation, Label
         import numpy as np
         
-        st.subheader('first plot')
-        fig = figure(width=500, height=300)
-        tapl2Fe3 = st.session_state.resultsFe3Smp[r'Fe$^{3+}$/$\Sigma$Fe (FP, TAP2)']
-        tapl4Fe3 = st.session_state.resultsFe3Smp[r'Fe$^{3+}$/$\Sigma$Fe (FP, TAP4)']
-        fig.scatter(tapl2Fe3, tapl4Fe3)
+        plwidth=400
+        plheight=250
+        
+        col1, col2, col3 = st.columns(3)
+        
+        col1.subheader('Sample measurements only')
+        fig = figure(width=plwidth, height=plheight)
+        tapl2Fe3 = st.session_state.resultsFe3Smp[r'Fe$^{3+}$/$\Sigma$Fe (2TAPL)']
+        tapl4Fe3 = st.session_state.resultsFe3Smp[r'Fe$^{3+}$/$\Sigma$Fe (4TAPL)']
+        fig.scatter(tapl2Fe3, tapl4Fe3, color='teal')
         
         x = np.linspace(0,1,10)
         fig.line(x, x)
@@ -609,39 +610,39 @@ def visualisations():
         
         fig.xaxis.axis_label=r"$$Fe^{3+} / \Sigma Fe \textrm{ (2TAPL)}$$"
         fig.yaxis.axis_label=r'$$Fe^{3+} / \Sigma Fe \textrm{ (4TAPL)}$$'
-        st.bokeh_chart(fig)
+        col1.bokeh_chart(fig)
+
         
-        
-        st.subheader('second plot')
-        fig = figure(width=500, height=300)
+        col1.subheader('All individual measurements')
+        fig = figure(width=plwidth, height=plheight)
         tapl2Betacps = st.session_state.dfMain[r'L$\beta$ (TAP2)']
         tapl2Alphacps = st.session_state.dfMain[r'L$\alpha$ (TAP2)']
         tapl4Betacps = st.session_state.dfMain[r'L$\beta$ (TAP4)']
         tapl4Alphacps = st.session_state.dfMain[r'L$\alpha$ (TAP4)']
         
-        fig.scatter(tapl2Betacps, tapl2Alphacps)
-        fig.scatter(tapl4Betacps, tapl4Alphacps)
+        fig.scatter(tapl2Betacps, tapl2Alphacps, legend_label='2TAPL')
+        fig.scatter(tapl4Betacps, tapl4Alphacps, color='olive', legend_label='4TAPL')
         fig.xaxis.axis_label=r'$$L\beta \textrm{ (net intensities)}$$'
         fig.yaxis.axis_label=r'$$L\alpha \textrm{ (net intensities)}$$'
         #ax2.legend()
-        st.bokeh_chart(fig)
+        col1.bokeh_chart(fig)
         
         
-        st.subheader('3')
-        fig = figure(width=500, height=300)
-        fig.line(st.session_state.dfMain['Point Nr.'], (tapl2Betacps/tapl2Alphacps)/(tapl4Betacps/tapl4Alphacps))
+        col3.subheader('All individual measurements')
+        fig = figure(width=plwidth, height=plheight)
+        fig.line(st.session_state.dfMain['Point Nr.'], (tapl2Betacps/tapl2Alphacps)/(tapl4Betacps/tapl4Alphacps), color='teal')
         fig.xaxis.axis_label='Point Nr.'
         fig.yaxis.axis_label=r'$$L\beta/L\alpha \textrm{ (2TAPL)} /L\beta/L\alpha \textrm{ (4TAPL)}$$'
-        st.bokeh_chart(fig)
+        col3.bokeh_chart(fig)
         
 
-        st.subheader('4')
-        fig = figure(width=500, height=300)
+        col3.subheader('All individual measurements')
+        fig = figure(width=plwidth, height=plheight)
         fig.scatter(st.session_state.dfMain['Point Nr.'], tapl2Betacps/tapl2Alphacps, legend_label='2TAPL')
         fig.scatter(st.session_state.dfMain['Point Nr.'], tapl4Betacps/tapl4Alphacps, color='olive', legend_label='4TAPL')
         fig.xaxis.axis_label='Point Nr.'
         fig.yaxis.axis_label=r'$$L\beta/L\alpha \textrm{ (counts-ratio)}$$'
-        st.bokeh_chart(fig)
+        col3.bokeh_chart(fig)
 
 
 #--------  End Comparing Lalpha & Lbeta
@@ -680,13 +681,24 @@ def visualisations():
         figParam.axis.minor_tick_out = 6
         
         st.bokeh_chart(figParam)
+        
+        st.markdown('<h4 style="color:black"><b>Regression formulas</b> </h4>', unsafe_allow_html=True)
+        st.latex(r'''Fe^{2+} = A + B \times \frac{L\beta}{L\alpha} + C \times \Sigma Fe + D \times \Sigma Fe \times \frac{L\beta}{L\alpha}''')
+        st.latex(r'''Fe^{3+} = -A - B \times \frac{L\beta}{L\alpha} - C \times \Sigma Fe - D \times \Sigma Fe \times \frac{L\beta}{L\alpha} + Fe_{tot}''')
+        st.latex(r'''\textrm{The result is } Fe^{2+} \textrm{ or } Fe^{3+} \textrm{, respectively, in wt\%} ''')
+        
+        st.markdown('<h4 style="color:black"><b>Calculated Fit Parameters</b> </h4>', unsafe_allow_html=True)
+        st.write(pd.DataFrame({'Parameter':['A', 'B', 'C', 'D'],
+                               'TAP2':st.session_state.fitParametersTAP2,
+                               'TAP4':st.session_state.fitParametersTAP4}))
+
     
 #-------- End Parametrisation
 
 #-------- Start Sample Inspection
 
     def sampleInspection(sel):
-        from bokeh.plotting import figure
+        from bokeh.plotting import figure, output_file, ColumnDataSource
         from bokeh.models import Span, BoxAnnotation, Label
         from bokeh.layouts import gridplot
         import numpy as np
@@ -724,7 +736,7 @@ def visualisations():
         if sel == 'Select one element, display all samples':       
             elements = st.session_state.dfMain.columns.tolist()[3:]
             el = st.selectbox('Select an Element', elements)
-            noc = st.number_input('Insert the Number of Plot Columns', value=4)
+            noc = st.number_input('Insert the Number of Plot Columns', value=3)
             
             plotList = []
             for i in st.session_state.dfSampleNames:
@@ -761,41 +773,71 @@ def visualisations():
             smpNames=st.session_state.dfSampleNames
             smp = st.selectbox('Select a Sample', smpNames)
             el = st.selectbox('Select an Element', elements)
+                                   
+            col1, col2 = st.columns([3, 1])
+            col1.subheader('Drift Monitor')
             
-            fil = (st.session_state.dfMain['Name'] == smp) & (st.session_state.dfMain[el])
-            xdata = st.session_state.dfMain[fil].loc[:, 'Point Nr.']
-            data = st.session_state.dfMain[fil].loc[:, el]
-            dat = (el, xdata, data)
+            dfMainFil = st.session_state.dfMain['Name'] == smp
+            smpSel = st.session_state.dfMain[dfMainFil]
             
-            st.bokeh_chart(plotStyle(dat))
+            av = np.average(smpSel[el])
+            std = np.std(smpSel[el])
+            reldev = 100 * np.std(smpSel[el])/np.average(smpSel[el])
+
+            TOOLTIPS = [('Name', '@Name'),
+                        ('Point Nr.', '@{Point Nr.}'),
+                        (el, '@'+el)]
+            
+            fig = figure(width=500, height=300, tooltips = TOOLTIPS)
+                        
+            fig.line(smpSel.loc[:, 'Point Nr.'], smpSel.loc[:, el])
+            #fig.circle(st.session_state.dfdr.loc[:, 'Point Nr.'], st.session_state.dfdr.loc[:, el])
+            output_file("toolbar.html")
+            source = ColumnDataSource(smpSel.to_dict('list'))
+            
+            fig.circle('Point Nr.', el, size=4, source=source)
+        
+            fig.xaxis.axis_label='Point Nr.'
+            fig.yaxis.axis_label=el + ' (wt%)'
+
+            av_hline = Span(location=av, dimension='width', line_dash='dashed', line_color='brown', line_width=2)
+            std_add_hline = Span(location=av+std, dimension='width', line_dash='dashed', line_color='brown', line_width=1)
+            std_sub_hline = Span(location=av-std, dimension='width', line_dash='dashed', line_color='brown', line_width=1)
+            fig.renderers.extend([av_hline, std_add_hline, std_sub_hline])
+            std_box = BoxAnnotation(bottom=av-std, top=av+std, fill_alpha=0.2, fill_color='yellow')
+            fig.add_layout(std_box)
+
+            col1.bokeh_chart(fig)
+            
+            
+            col2.subheader('Statistics')
+            resAv = 'average: ' + str(round(av, 2)) + '±' + str(round(std, 2))
+            resRelStd ='rel. std.: ' + str(round(reldev, 2)) + '%'
+            col2.write(resAv)
+            col2.write(resRelStd)
+            if reldev < 1:
+                col2.color_picker('good data', '#39EC39')
+            elif 1 <= reldev < 5:
+                col2.color_picker('check data', '#F7CF0F')
+            else:
+                col2.color_picker('worrisome data', '#FF0000')
+                
 
 #-------- End Sample Inspection
 
 #--------  Start Error Considerations
 
     def errorConsiderations():
-        st.write('result variations:\
-        Individual samples are plotted along the x-axis.\
-        For each sample, the Fetot (top 2 plots) and\
-        Lbeta/Lalpha (bottom 2 plots) are changed by the percentage\
-        given in the legend. The Fe3+/SumFe is then calculated with\
-        the new Fetot or Lbeta/Lalpha. The result is then subtracted\
-        from the true Fe3+/SumFe and plotted on the y-axis.\
-        sample s.d.:\
-        Individual samples/drift monitors are plotted along the x-axis.\
-        The 1 s.d. of Lbeta/Lalpha of a single sample is calculated and\
-        plotted on the y-axis.')
-        
                     
-        ##-----------------------------------------------##
-        ##-------------  result variations --------------##
-        ##-----------------------------------------------##
+    ##-----------------------------------------------##
+    ##-------------  result variations --------------##
+    ##-----------------------------------------------##
         
         def errorPercentDeviations():
-            global yData1
-            global yData2
-            global yData3
-            global yData4
+            #global yData1
+            #global yData2
+            #global yData3
+            #global yData4
             
             fig = plt.figure(figsize = (10, 7))
             gs = fig.add_gridspec(2, 2, hspace=0, wspace=0)
@@ -854,10 +896,10 @@ def visualisations():
             plt.show()
         
         
-        ##-----------------------------------------------##
-        ##---------------  sample s.d.  ----------------##
-        ##-----------------------------------------------##
-        
+    ##-----------------------------------------------##
+    ##---------------  sample s.d.  ----------------##
+    ##-----------------------------------------------##
+    
         def errorSmpFe3Dev():
             fig = plt.figure(figsize = (10, 3))
             gs = fig.add_gridspec(1, 2, hspace=0, wspace=0)
@@ -902,7 +944,7 @@ def visualisations():
     
     if plotSel == 'Drift Inspection':
         st.subheader('Drift Inspection')
-        sel = st.selectbox('Select', ('elements', 'Fe3+'))
+        sel = st.radio('Choose what to inspect', ('Composition of drift standards', 'Fe3+ using 2TAPL vs. Fe3+ using 4TAPL'), horizontal=True)
         driftplots(sel)
     elif plotSel == 'Comparing La & Lb':
         st.subheader('Comparing La & Lb')
@@ -922,19 +964,101 @@ def visualisations():
     with st.sidebar:
         with st.expander("Instructions for this site"):
          st.write("""
+             Use the various visualisation tools to analyse your data and optimise your upload file. 
+             Check the 'Tutorials & Instructions' resource on how to do this.
+         """)
+         
+    with st.sidebar:
+        with st.expander("Info Drift Inspection"):
+         st.write("""
+             Check the composition of the dirft monintor measurements and identify the stability of/variations duringduring the measurement campaign.
+             Also check how the Fe3+ abundances of the two analyser crystals compare.
+         """)
+                  
+    with st.sidebar:
+        with st.expander("Info Comparing La & Lb"):
+         st.write("""
+             The plots provide insights to potential issues during the measurements.
+         """)
+                  
+    with st.sidebar:
+        with st.expander("Info Parametrisation"):
+         st.write("""
+             The plot visualises the formula, with which the Fe3+ in the samples are calculated.
+         """)
+         
+    with st.sidebar:
+        with st.expander('Info Sample Inspection'):
+            st.write("""
+                This provides comprehensive possibilities to check the composition of all samples in various overviews to high granularity. 
+            """)
+            
+    with st.sidebar:
+        with st.expander('Info Error Considerations'):
+            st.write("""
+                result variations:\
+        Individual samples are plotted along the x-axis.\
+        For each sample, the Fetot (top 2 plots) and\
+        Lbeta/Lalpha (bottom 2 plots) are changed by the percentage\
+        given in the legend. The Fe3+/SumFe is then calculated with\
+        the new Fetot or Lbeta/Lalpha. The result is then subtracted\
+        from the true Fe3+/SumFe and plotted on the y-axis.\
+        sample s.d.:\
+        Individual samples/drift monitors are plotted along the x-axis.\
+        The 1 s.d. of Lbeta/Lalpha of a single sample is calculated and\
+        plotted on the y-axis.
+            """)
+
+
+#-----------------------------------------#
+#------------ Start Output ---------------#
+#-----------------------------------------#
+def outputForm():
+    
+    with st.sidebar:
+        with st.expander("Instructions for this site"):
+         st.write("""
              1- Choose which data shall be reduced. 2- Select the standards used to claculte the fit parameters. 
              Note that you can only choose those also present in the Moessbauer Standard Data file. 3- Click on 'Calculate Results'. 
              4- You can change your selection of standards or whether to use all or only the inspected data anytime.
              However, after each change you need to click 'Calculate Results' again.
              5- Proceed to 'Result Tables'.
          """)
-         
+    
+    st.markdown(f"# {list(page_names_to_funcs.keys())[5]}")
+    st.write(
+        """
+        This demo shows how to use `st.write` to visualize Pandas DataFrames.
+
+(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)
+"""
+    )
+                  
+
+
+#-----------------------------------------#
+#------------ Start Output ---------------#
+#-----------------------------------------#
+def outputForm():
+    
     with st.sidebar:
-        with st.expander("Info Drift Inspection"):
+        with st.expander("Instructions for this site"):
          st.write("""
-             xxx
+             1- Choose which data shall be reduced. 2- Select the standards used to claculte the fit parameters. 
+             Note that you can only choose those also present in the Moessbauer Standard Data file. 3- Click on 'Calculate Results'. 
+             4- You can change your selection of standards or whether to use all or only the inspected data anytime.
+             However, after each change you need to click 'Calculate Results' again.
+             5- Proceed to 'Result Tables'.
          """)
-         
+    
+    st.markdown(f"# {list(page_names_to_funcs.keys())[5]}")
+    st.write(
+        """
+        This demo shows how to use `st.write` to visualize Pandas DataFrames.
+
+(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)
+"""
+    )
 
 
 #-----------------------------------------#
@@ -1030,6 +1154,6 @@ page_names_to_funcs = {
     'Method & References': method
 }
 
-demo_name = st.sidebar.radio("Go through your data analysis here", page_names_to_funcs.keys())
+demo_name = st.sidebar.radio("Start your flank method analysis journey here", page_names_to_funcs.keys())
 page_names_to_funcs[demo_name]()
 
