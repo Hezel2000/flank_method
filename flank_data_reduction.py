b50a67ff3da9677088af7202305c176986aa6723
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2022-06-24 17:11:26 +0200

    Update flank_data_reduction.py

--- flank_data_reduction.py
+++ flank_data_reduction.py
@@ -3,14 +3,14 @@
 
 import streamlit as st
 
-#hide_st_style = """
-#            <style>
-#            #MainMenu {visibility: hidden;}
-#            footer {visibility: hidden;}
-#            header {visibility: hidden;}
-#            </style>
-#            """
-#st.markdown(hide_st_style, unsafe_allow_html=True)
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 #-----------------------------------------#
