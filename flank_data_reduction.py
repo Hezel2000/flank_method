455e439ff962cf37f06738362b0830760d28ff2e
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2022-07-12 21:47:27 +0200

    Update flank_data_reduction.py

--- flank_data_reduction.py
+++ flank_data_reduction.py
@@ -387,7 +387,8 @@ def dataReduction():
         st.session_state.stdSelection = st.multiselect('', allSmpNames, allSmpNames[:4])
         st.session_state.drMonitorName = st.selectbox('Further select the standard used as drift monitor.', st.session_state.dfMoess['Name'])
         st.session_state.selMoessData = st.selectbox('Finally select the Moessbauer values to be used.', st.session_state.dfMoess['Name'])
-        st.write(st.session_state.dfMoess.columns.tolist()[1,3,5])
+        tmp = st.write(st.session_state.dfMoess.columns.tolist())
+        st.write(tmp[1,3,5])
         #st.write('You selected: ', st.session_state.stdSelection)
             
     if st.button('Calculate Results'):
