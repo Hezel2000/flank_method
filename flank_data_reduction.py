9102ab55ae0ca08d77cf9242a2222a1194a3ab68
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-02-15 20:06:16 +0100

    x

--- flank_data_reduction.py
+++ flank_data_reduction.py
@@ -92,7 +92,7 @@ def dataUpload():
 #------------ Start Data Reduction -------#
 #-----------------------------------------#
 
-@st.cache_data
+#@st.cache_data
 def dataReduction():
     import pandas as pd
 
