36073aa375b459dc99715d0e43bfb4f811e6f34b
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-02-16 16:17:31 +0100

    x

--- Welcome.py
+++ Welcome.py
@@ -55,5 +55,3 @@ with st.sidebar:
         st.write("""
         Such boxes on each page contain information about how to use the according page.
         """)
-
-st.markdown(':red[**This is an entirely new version (16.02.2023), with a lot of technical changes! Please let me know asap if something is not working properly**]')
