c1560f4956ad136d46a4304f609af87dfb5cbede
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-03-07 07:22:52 +0100

    Update 05_Dev.py

--- pages/05_Dev.py
+++ pages/05_Dev.py
@@ -1,28 +1,37 @@
-def sess_states():
+def tools_info_2():
     import streamlit as st
 
-    st.write('test')
-
-    # st.dataframe(st.session_state.dfFitData)
-
-    # st.write('resultsFe3Smp')
-    # st.session_state.resultsFe3Smp
-
-    # st.write('resultsFe3Std')
-    # st.session_state.resultsFe3Std
-
-    # # st.write('smpAndstdList')
-    # # st.session_state.smpAndstdList
-
-    # st.write('Fe3SmpAndFe3Std_2')
-    # fil = st.session_state.resultsFe3Smp['Name'].isin(
-    #     st.session_state.dfMoessNames)
-    # st.session_state.Fe3SmpAndFe3Std_2 = pd.concat([
-    #     st.session_state.resultsFe3Smp[~fil], st.session_state.resultsFe3Std])
-    # st.session_state.Fe3SmpAndFe3Std_2
-
-    # st.write('dfMain')
-    # st.session_state.dfMain
+    toolSel = st.sidebar.radio(
+        'Select Tool/Info', ('Tutorials & Documentations', 'Crystal Positioning', 'Result Tables', 'Check Data Integrity', 'Calculate individual Fe2+ & Fe3+', 'Downloads'))
 
 
-sess_states
+tools_info_2()
+
+# def sess_states():
+#     import streamlit as st
+
+#     st.write('test')
+
+#     # st.dataframe(st.session_state.dfFitData)
+
+#     # st.write('resultsFe3Smp')
+#     # st.session_state.resultsFe3Smp
+
+#     # st.write('resultsFe3Std')
+#     # st.session_state.resultsFe3Std
+
+#     # # st.write('smpAndstdList')
+#     # # st.session_state.smpAndstdList
+
+#     # st.write('Fe3SmpAndFe3Std_2')
+#     # fil = st.session_state.resultsFe3Smp['Name'].isin(
+#     #     st.session_state.dfMoessNames)
+#     # st.session_state.Fe3SmpAndFe3Std_2 = pd.concat([
+#     #     st.session_state.resultsFe3Smp[~fil], st.session_state.resultsFe3Std])
+#     # st.session_state.Fe3SmpAndFe3Std_2
+
+#     # st.write('dfMain')
+#     # st.session_state.dfMain
+
+
+# sess_states
