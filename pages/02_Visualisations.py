c75286421d2314baf37d6311cb0c312cc2efcba2
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-03-07 08:37:57 +0100

    Update 02_Visualisations.py

--- pages/02_Visualisations.py
+++ pages/02_Visualisations.py
@@ -76,7 +76,7 @@ def visualisations():
                           (BTAP2 + DTAP2 * Fetot), line_color='blue', line_alpha=.3)
             figParam.line(Fetot, (-ATAP4 - CTAP4 * Fetot + Fetot - Fetot * Fe3) /
                           (BTAP4 + DTAP4 * Fetot), line_color='orange', line_alpha=.3)
-        glyph = Text(x='50', y='1', text="text", text_color="#96deb3")
+        glyph = Text(x='50', y='1', text="text", text_color='black')
         figParam.add_glyph(glyph)
 
         figParam.circle(st.session_state.dfMeasSmpDataTAP2[r'Fe$_{tot}$'], st.session_state.dfMeasSmpDataTAP2[r'L$\beta$/L$\alpha$ (TAP2)'],
