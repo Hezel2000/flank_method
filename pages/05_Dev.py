1d6f04a4c02f89d7c5931b17db7389f189a261ed
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-03-07 07:19:13 +0100

    Update 05_Dev.py

--- pages/05_Dev.py
+++ pages/05_Dev.py
@@ -6,24 +6,24 @@ def sess_states():
 
     st.dataframe(st.session_state.dfFitData)
 
-    st.write('resultsFe3Smp')
-    st.session_state.resultsFe3Smp
+    # st.write('resultsFe3Smp')
+    # st.session_state.resultsFe3Smp
 
-    st.write('resultsFe3Std')
-    st.session_state.resultsFe3Std
+    # st.write('resultsFe3Std')
+    # st.session_state.resultsFe3Std
 
-    # st.write('smpAndstdList')
-    # st.session_state.smpAndstdList
+    # # st.write('smpAndstdList')
+    # # st.session_state.smpAndstdList
 
-    st.write('Fe3SmpAndFe3Std_2')
-    fil = st.session_state.resultsFe3Smp['Name'].isin(
-        st.session_state.dfMoessNames)
-    st.session_state.Fe3SmpAndFe3Std_2 = pd.concat([
-        st.session_state.resultsFe3Smp[~fil], st.session_state.resultsFe3Std])
-    st.session_state.Fe3SmpAndFe3Std_2
+    # st.write('Fe3SmpAndFe3Std_2')
+    # fil = st.session_state.resultsFe3Smp['Name'].isin(
+    #     st.session_state.dfMoessNames)
+    # st.session_state.Fe3SmpAndFe3Std_2 = pd.concat([
+    #     st.session_state.resultsFe3Smp[~fil], st.session_state.resultsFe3Std])
+    # st.session_state.Fe3SmpAndFe3Std_2
 
-    st.write('dfMain')
-    st.session_state.dfMain
+    # st.write('dfMain')
+    # st.session_state.dfMain
 
 
 sess_states
