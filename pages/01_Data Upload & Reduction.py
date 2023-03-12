5e3063b5efe77c3d16fb57d719be6f3f499cb3ad
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-03-12 17:31:24 +0100

    x

--- pages/01_Data Upload & Reduction.py
+++ pages/01_Data Upload & Reduction.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 
+st.session_state.dfRaw = 0
 
 def dataUpload():
     import streamlit as st
@@ -11,7 +12,7 @@ def dataUpload():
         #st.session_state.dfRaw = pd.read_csv(uploaded_file)
         st.session_state.dfRaw = pd.read_csv(uploaded_file, sep=";|,", engine="python")
 
-    if st.session_state.dfRaw is None:
+    if st.session_state.dfRaw == 0:
         st.write('Nothing uploaded yet')
     else:
         if st.session_state.dfRaw.columns.tolist()[0] == 'Unnamed: 0':
