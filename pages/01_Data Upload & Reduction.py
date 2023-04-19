3fdc6eacb1e765dadb1fbbd605bab071306ba5d8
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-04-19 10:36:29 +0200

    x

--- pages/01_Data Upload & Reduction.py
+++ pages/01_Data Upload & Reduction.py
@@ -18,6 +18,9 @@ def dataUpload():
         if st.session_state.dfRaw.columns.tolist()[0] == 'Unnamed: 0':
             st.session_state.dfRaw.drop(
                 st.session_state.dfRaw.columns[0], axis=1, inplace=True)
+        
+        fil = st.session_state.dfRaw['Inspected'] == 'ignore'
+        st.session_state.dfRaw = st.session_state.dfRaw[~fil]
 
         with st.expander('You uploaded the following data for flank reduction'):
             st.dataframe(st.session_state.dfRaw)
@@ -342,9 +345,9 @@ def calcRegressionsAndProduceResults(selMoessData):
 @st.cache_data
 def importMoessStdFile():
     import pandas as pd
-    # return pd.read_csv('data/moessbauer standard data.csv')
-    return pd.read_csv(
-        'https://raw.githubusercontent.com/Hezel2000/flank_method/main/data/moessbauer%20standard%20data.csv')
+    return pd.read_csv('data/moessbauer standard data.csv')
+    # return pd.read_csv(
+    #     'https://raw.githubusercontent.com/Hezel2000/flank_method/main/data/moessbauer%20standard%20data.csv')
 
 
 with st.sidebar:
