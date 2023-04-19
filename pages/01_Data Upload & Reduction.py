045dd4a5fb68f7907c8e9c98915e086382f459ed
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-04-19 10:55:14 +0200

    x

--- pages/01_Data Upload & Reduction.py
+++ pages/01_Data Upload & Reduction.py
@@ -21,7 +21,7 @@ def dataUpload():
         
         fil = (st.session_state.dfRaw_input['Inspected'] == 'ignore') | (st.session_state.dfRaw_input['Inspected'] == 'Ignore')
         st.session_state.dfRaw = st.session_state.dfRaw_input[~fil]
-        st.session_state.dfRaw.reindex()
+        st.session_state.dfRaw.reset_index()
 
         with st.expander('You uploaded the following data for flank reduction'):
             st.dataframe(st.session_state.dfRaw)
