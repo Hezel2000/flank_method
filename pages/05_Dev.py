9d8e4c40cc9ef8fd51f06101e9054620791a142f
Dominik Hezel <dominik.hezel@em.uni-frankfurt.de>
2023-04-20 21:59:56 +0200

    x

--- pages/05_Dev.py
+++ pages/05_Dev.py
@@ -1,4 +1,8 @@
 import streamlit as st
+st.session_state.dfMoess[st.session_state.dfMoess['Name'].str.contains(
+        'AlmO')]
+st.session_state.dfMoess[st.session_state.dfMoess['Name'].str.contains(
+        'AND1')]
 st.session_state.dfMoess
 st.session_state
 
