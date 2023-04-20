57fa5d42a704210b7ea8954712fa52f48a332976
Dominik Hezel <dominik.hezel@em.uni-frankfurt.de>
2023-04-20 22:05:49 +0200

    x

--- pages/01_Data Upload & Reduction.py
+++ pages/01_Data Upload & Reduction.py
@@ -214,6 +214,7 @@ def regressionFitParameters(inpData, crystal):
 def extractKnownFe2(stdNameForMatching):
     foundStd = st.session_state.dfMoess[st.session_state.dfMoess['Name'].str.contains(
         stdNameForMatching)]
+    st.write(foundStd)
     Fe2ModAbValue = foundStd['Fe2+/SumFe'].tolist()[0]
     st.write(Fe2ModAbValue)
     return Fe2ModAbValue
