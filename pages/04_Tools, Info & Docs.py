ded802c3a6666370f78c90036ee317b6828df2b9
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-10-13 08:08:07 +0200

    x

--- pages/04_Tools, Info & Docs.py
+++ pages/04_Tools, Info & Docs.py
@@ -84,6 +84,7 @@ def crystalPositioning():
         fig.yaxis.axis_label_text_font_size = "14pt"
         fig.yaxis.major_label_text_font_size = "14pt"
         fig.legend.label_text_font_size = "14pt"
+        fig.legend.location = "top_left"
         # fig.y_range = Range1d(-500, 1300)
         fig.add_layout(fig.legend[0], 'below')
 
